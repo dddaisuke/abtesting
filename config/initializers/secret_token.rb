9f03c803935653c04d2565eb3058558f18cc20df
Daisuke Yamashita <daisuke@cookpad.com>
2013-02-20 11:41:17 +0900

    rails new abtesting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abtesting::Application.config.secret_token = 'b03915a228215f0ec0d474bd874ae8efee72e5716ad2519df90bbc1a00f867522312bd611df5b6c90b7bd3c760aeb7d96133bcd58796d701b8f3a9868f63a6bb'
